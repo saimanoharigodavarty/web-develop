dd428e7067455fab7a5a37c20097562dec072116
saimanohari <saimanoharigodavarty06@gmail.com>
2026-05-11 13:07:46 +0530

    Namw change

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -53,7 +53,7 @@ const Home = () => {
               marginTop: "1rem",
             }}
           >
-            Your App Name
+            Sai Manohari Godavarty
           </Title>
 
           <Text
